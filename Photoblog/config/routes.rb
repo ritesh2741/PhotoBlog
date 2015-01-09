606b543ac1b20eadf2a87b071ca25b8970a3dd8f
razan123 <rajan4ualwz@hotmail.com>
2015-01-09 10:24:59 +0545

    Index Page rendered

--- Photoblog/config/routes.rb
+++ Photoblog/config/routes.rb
@@ -32,12 +32,12 @@ Photoblog::Application.routes.draw do
   #   end
 
   # Sample resource route with more complex sub-resources
-  #   resources :products do
+     resources :posts do
   #     resources :comments
   #     resources :sales do
   #       get 'recent', :on => :collection
   #     end
-  #   end
+    end
 
   # Sample resource route within a namespace:
   #   namespace :admin do
@@ -48,7 +48,7 @@ Photoblog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
