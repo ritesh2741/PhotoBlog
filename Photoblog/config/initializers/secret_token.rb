ae4f0e170d06be5c35a94e3771f2616d66708a69
razan123 <rajan4ualwz@hotmail.com>
2015-01-09 09:45:48 +0545

    New Project Initiation

---
+++ Photoblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoblog::Application.config.secret_token = 'a5d844b9dc4cde1b3951eb544b0a20637647d71aa994d091b82cf7f7a70df2d0d4c3347e1db47a2702c3eb7f4decff59721dff97fe53dc9f3e098efdca0a0802'
